7244a9b9119aa715e2e0fcb39fc5fbb8017f4764
JosephLee-lffy <JosephLee-lffy@outlook.com>
2026-05-07 08:04:36 +0800

    上传ex02

---
+++ ex02/Development_Environment_Testing/Development_Environment_Testing.ino
@@ -0,0 +1,12 @@
+#define LED_PIN 2
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+}
+void loop() {
+  Serial.println("Hello ESP32!");
+  digitalWrite(LED_PIN, HIGH);
+  delay(200);
+  digitalWrite(LED_PIN, LOW);
+  delay(200);
+}
